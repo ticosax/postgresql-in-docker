1e3744465fdb07269fae7e83573ed3103f6a9a61
Nicolas Delaby <nicolas.delaby@ezeep.com>
2014-02-27 17:24:11 +0100

    fix quote mistake

--- start-postgres.sh
+++ start-postgres.sh
@@ -5,6 +5,6 @@ chown -R postgres: /var/lib/postgresql/9.3/main
 chown -R postgres: /var/log/postgresql
 if [ ! "$(ls -A /var/lib/postgresql/9.3/main)" ]; then
     su postgres -c "/usr/lib/postgresql/9.3/bin/initdb /var/lib/postgresql/9.3/main"
-    su postgres -c "/usr/lib/postgresql/9.3/bin/postgres --single -c config-file=/etc/postgresql/9.3/main/postgresql.conf" <<< "alter user postgres with password '$PG_PASSWORD';"
+    su postgres -c "/usr/lib/postgresql/9.3/bin/postgres --single -c config-file=/etc/postgresql/9.3/main/postgresql.conf <<< 'alter user postgres with password '\''$PG_PASSWORD'\'';'"
 fi
 su postgres -c "/usr/lib/postgresql/9.3/bin/postgres -c config-file=/etc/postgresql/9.3/main/postgresql.conf -c listen-addresses=*" & wait $!
