efcf96c515f8cbd9f45e9fb801bec23437ee260a
Nicolas Delaby <nicolas.delaby@ezeep.com>
2014-03-07 12:04:19 +0100

    normalize locale.

--- postgresql.sh
+++ postgresql.sh
@@ -5,7 +5,7 @@ chown -R postgres: /var/lib/postgresql/9.3/main
 chown -R postgres: /var/log/postgresql
 chmod 700 /var/lib/postgresql/9.3/main
 if [ ! "$(ls -A /var/lib/postgresql/9.3/main)" ]; then
-    su postgres -c "/usr/lib/postgresql/9.3/bin/initdb -E utf8 --locale=en_US.utf-8 /var/lib/postgresql/9.3/main"
+    su postgres -c "/usr/lib/postgresql/9.3/bin/initdb -E utf8 --locale=en_US.UTF-8 /var/lib/postgresql/9.3/main"
     su postgres -c "/usr/lib/postgresql/9.3/bin/postgres --single -c config-file=/etc/postgresql/9.3/main/postgresql.conf <<< 'alter user postgres with password '\''$PG_PASSWORD'\'';'"
 fi
 exec /sbin/setuser postgres /usr/lib/postgresql/9.3/bin/postgres -c config-file=/etc/postgresql/9.3/main/postgresql.conf -c listen-addresses=*
